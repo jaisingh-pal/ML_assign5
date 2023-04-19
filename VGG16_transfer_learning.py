843e8c209fa43182f64087267137eaf0408a52e3
Radhika-Joglekar <122112459+Radhika-Joglekar@users.noreply.github.com>
2023-04-19 19:15:26 +0530

    Update VGG16_transfer_learning.py

--- VGG16_transfer_learning.py
+++ VGG16_transfer_learning.py
@@ -1,1 +1,66 @@
 
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision.datasets import CIFAR10
+import torchvision.transforms as transforms
+from torch.utils.tensorboard import SummaryWriter
+from keras.callbacks import TensorBoard
+# vgg16 model used for transfer learning on the dogs and cats dataset
+import sys
+from matplotlib import pyplot
+from keras.utils import to_categorical
+from keras.applications.vgg16 import VGG16
+from keras.models import Model
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.optimizers import SGD
+from keras.preprocessing.image import ImageDataGenerator
+
+# define cnn model
+def define_model():
+	# load model
+	model = VGG16(include_top=False, input_shape=(224, 224, 3))
+	# mark loaded layers as not trainable
+	for layer in model.layers:
+		layer.trainable = False
+	# add new classifier layers
+	flat1 = Flatten()(model.layers[-1].output)
+	class1 = Dense(128, activation='relu', kernel_initializer='he_uniform')(flat1)
+	output = Dense(1, activation='sigmoid')(class1)
+	# define new model
+	model = Model(inputs=model.inputs, outputs=output)
+	# compile model
+	opt = SGD(lr=0.001, momentum=0.9)
+	model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+	return model
+
+# run the test harness for evaluating a model
+def run_test_harness():
+	# define model
+	model = define_model()
+	# create data generator
+	datagen = ImageDataGenerator(featurewise_center=True)
+	# specify imagenet mean values for centering
+	datagen.mean = [123.68, 116.779, 103.939]
+	# prepare iterator
+	train_it = datagen.flow_from_directory('/content/drive/MyDrive/ML_after_preprocessing/train/',
+		class_mode='binary', batch_size=64, target_size=(224, 224))
+	test_it = datagen.flow_from_directory('/content/drive/MyDrive/ML_after_preprocessing/test/',
+		class_mode='binary', batch_size=64, target_size=(224, 224))
+ # define callbacks
+	tb_callback = TensorBoard(log_dir='./logs', histogram_freq=1)
+	# fit model
+	history = model.fit_generator(train_it, steps_per_epoch=len(train_it),
+		validation_data=test_it, validation_steps=len(test_it), epochs=10, verbose=1, callbacks=[tb_callback])
+	# evaluate model
+	_, acc = model.evaluate_generator(test_it, steps=len(test_it), verbose=0)
+	print('> %.3f' % (acc * 100.0))
+
+
+# entry point, run the test harness
+run_test_harness()
+
+writer = SummaryWriter('runs/vgg1')
+%tensorboard --logdir=/content/logs
